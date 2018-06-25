2db44ea47b0836316c11ed85b356db590cfc75c1
zhengxiaobin <zhengxiaobin@allwinnertech.com>
2018-06-25 11:34:11 +0800

    add pan display test program

---
+++ framebuffer/fb_pan_display_test.c
@@ -0,0 +1,49 @@
+#include "fbcommon.h"
+
+int main(int argc, char *argv[])
+{
+	int ret = 1;
+	struct fb_object *drawfb = NULL;
+	int x = 0;
+
+	ret = fb_object_init(&drawfb, 0); /*/dev/fb0*/
+	if (ret || !drawfb)
+		goto OUT;
+
+	ret = drawfb->fb_device_open(drawfb);
+	if (ret < 0)
+		goto FREE_FB;
+
+	ret = drawfb->fb_device_get_finfo(drawfb);
+
+	drawfb->print_fixed_info(drawfb);
+
+	ret = drawfb->fb_device_get_vinfo(drawfb);
+
+	drawfb->print_var_info(drawfb);
+
+	ret = drawfb->fb_device_mmap(drawfb);
+	if (ret || !drawfb->framebuffer)
+		goto FREE_FB;
+
+
+	while(x < drawfb->vinfo.yres) {
+		drawfb->vinfo.xoffset = 0;
+		drawfb->vinfo.yoffset = x;
+		x+=5;
+		drawfb->fb_device_pan_dispaly(drawfb);
+	}
+
+	while(x >= 0) {
+		drawfb->vinfo.xoffset = 0;
+		drawfb->vinfo.yoffset = x;
+		x-=5;
+		drawfb->fb_device_pan_dispaly(drawfb);
+	}
+
+	ret = 0;
+FREE_FB:
+	drawfb->fb_object_free(drawfb);
+OUT:
+	return ret;
+}
